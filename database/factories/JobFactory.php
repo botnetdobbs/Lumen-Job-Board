4e78fe7e1ae4202d200028b7447d8c8e7dea2b43
Lazarus Odhiambo <lazarusodhis@gmail.com>
2019-06-30 16:19:55 +0300

    ch (seeders): add model seeders

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$factory->define(App\Job::class, function (Faker\Generator $faker) {
+    return [
+        'title' => 'Software Developer',
+        'company' => 'Westwood inc',
+        'location' => 'Nairobi',
+        'job_description' => 'Expected to play a key role in the design, installation, testing and maintenance of our software systems',
+        'application_email' => 'applications@westwood.inc'
+    ];
+});
