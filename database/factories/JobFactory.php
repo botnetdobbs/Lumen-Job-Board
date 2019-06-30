2dbf1ea02961703f991057ff46d21916162692af
Lazarus Odhiambo <lazarusodhis@gmail.com>
2019-06-30 16:30:42 +0300

    Update seeders

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -6,6 +6,7 @@ $factory->define(App\Job::class, function (Faker\Generator $faker) {
         'title' => 'Software Developer',
         'company' => 'Westwood inc',
         'location' => 'Nairobi',
+        'employer_id' => 1,
         'job_description' => 'Expected to play a key role in the design, installation, testing and maintenance of our software systems',
         'application_email' => 'applications@westwood.inc'
     ];
